113fed945ce08405c4eead998a0b9ecb9350edc9
Masdon Malau <145627072+masdon554@users.noreply.github.com>
2024-04-27 13:36:29 +0700

    v.1.1.0

---
+++ copy.js
@@ -0,0 +1,25 @@
+function copyBTC() {  
+    var copyText = document.getElementById("copy-btc");  
+    copyText.select();  
+    document.execCommand("copy");
+}
+function copyETH() {  
+    var copyText = document.getElementById("copy-eth");  
+    copyText.select();  
+    document.execCommand("copy");
+}
+function copyBEP8() {  
+    var copyText = document.getElementById("copy-bep8");  
+    copyText.select();  
+    document.execCommand("copy");
+}
+function copyBEP20() {  
+    var copyText = document.getElementById("copy-bep20");  
+    copyText.select();  
+    document.execCommand("copy");
+}
+function copyTRX() {  
+    var copyText = document.getElementById("copy-trx");  
+    copyText.select();  
+    document.execCommand("copy");
+}
